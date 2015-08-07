5ea9588a4a4e8252482e8893a4e13bdcfb554076
ithan <ithan840123@gmail.com>
2015-08-07 12:04:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ithan/parallaxscrollexample/MultipleParallaxExpandableListView.java
@@ -0,0 +1,24 @@
+package com.example.ithan.parallaxscrollexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.nirhart.parallaxscroll.views.ParallaxExpandableListView;
+
+public class MultipleParallaxExpandableListView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.expand_list_multiple_parallax);
+		ParallaxExpandableListView listView = (ParallaxExpandableListView) findViewById(R.id.list_view);
+		CustomExpandableListAdapter adapter = new CustomExpandableListAdapter(LayoutInflater.from(this));
+		listView.setAdapter(adapter);
+	}
+}
